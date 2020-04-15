68a8aa859f04f6d8ac6e3980a48e3df1255656b5
baidurik <baidurik.media.official@gmail.com>
2020-04-15 15:53:23 +0900

    Added some templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Baidurik Media',
+        'title': 'My wishes',
+        'content': 'I want to get my ass fucked hardly',
+        'date_posted': 'April 15, 2017'
+    },
+    {
+        'author': 'Semyon Sychev',
+        'title': 'Hate you people',
+        'content': 'You all are gays',
+        'date_posted': 'May 5, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About page'})
